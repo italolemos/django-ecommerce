bfccd39e93f8b96837b5ca53299ecc5928469276
Ítalo Lemos <italovrlemos@gmail.com>
2017-12-15 00:08:02 -0300

    Admin settings

--- catalog/models.py
+++ catalog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
         verbose_name_plural = 'Categorias'
         ordering = ['name']
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField('Nome', max_length=100)
