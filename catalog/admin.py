a4d79c1c2e88735c4a3e40e285cc9be5e29524a5
Ítalo Lemos <italovrlemos@gmail.com>
2017-12-18 00:20:24 -0300

    admin changes

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Product, Category
 
-admin.site.register(Product)
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created', 'modified']
+    search_fields = ['name', 'slug']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created', 'modified']
+    search_fields = ['name', 'slug', 'category__name']
+    list_filter = ['created', 'modified']
+    actions = ['']
+
+
+admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Category, CategoryAdmin)
