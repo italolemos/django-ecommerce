f9f813d5684eabc14aa352673ff85a73e9466053
Ítalo Lemos <italovrlemos@gmail.com>
2018-01-05 16:01:24 -0300

    Paginação

--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,7 @@ from .models import Product, Category
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/product_list.html'
-
+    paginate_by = 3
 
 # def product_list(request):
 #     context = {
@@ -15,10 +15,12 @@ class ProductListView(ListView):
 #     }
 #     return render(request, 'catalog/product_list.html', context)
 
+
 class CategoryListView(ListView):
 
     template_name = 'catalog/category.html'
     context_object_name = 'product_list'
+    paginate_by = 3
 
     def get_queryset(self):
         return Product.objects.filter(category__slug=self.kwargs['slug'])
