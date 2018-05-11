4a4d0b9e48f8c291f31813e03813328e64231b96
Ítalo Lemos <italo_vinicius13@yahoo.com.br>
2018-05-11 15:21:12 -0300

    Django message system
    add message feature

--- market/settings.py
+++ market/settings.py
@@ -140,3 +140,14 @@ AUTHENTICATIONS_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'accounts.backends.ModelBackend',
 )
+
+# Messagens
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
